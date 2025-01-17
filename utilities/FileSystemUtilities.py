1ca89ed53988af498a1c744f0b1e47a11a876024
Samuel Kirk <70180845+sckirk1@users.noreply.github.com>
2025-01-16 18:40:12 -0600

    Update FileSystemUtilities.py

--- utilities/FileSystemUtilities.py
+++ utilities/FileSystemUtilities.py
@@ -6,3 +6,4 @@ __PATH_TO_OUTPUT_DIRECTORY = '../../out'
 def createOutDirectoryIfNotPresent():
     if not os.path.exists(__PATH_TO_OUTPUT_DIRECTORY):
         os.mkdir(__PATH_TO_OUTPUT_DIRECTORY)
+        return 1
